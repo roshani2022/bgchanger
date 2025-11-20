ab0e1a735967bfe17e7319d75083cde44a0d8b91
roshani2022 <rosh.gupta17@gmail.com>
2025-11-20 17:08:11 +0530

    feat(ui): add three buttons for background-colour change

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 
 
 
+
 function App() {
   
   return (
-    <>
-      
-    </>
+    <div className='w-full h-screen duration-200'>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button className='bg-green-900'>Green</button>
+           <button className='bg-pink-950'>Pink</button>
+            <button className='bg-purple-800'>Purple</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
