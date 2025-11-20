3b2548a53ff0ef6e709e46240681ae7ae22ff3e2
roshani2022 <rosh.gupta17@gmail.com>
2025-11-20 17:26:37 +0530

    add logic with use state to change background

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import { useState } from 'react'
 
 
 function App() {
-  
+    
+  const [color,setColor]  = useState('bg-cyan-900')
+
   return (
-    <div className='w-full h-screen duration-200'>
+    <div className={`w-full h-screen duration-200 ${color}`}>
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-          <button className='bg-green-900'>Green</button>
-           <button className='bg-pink-950'>Pink</button>
-            <button className='bg-purple-800'>Purple</button>
+          <button className='bg-green-900' onClick={()=>setColor("bg-green-900")}>Green</button>
+           <button className='bg-pink-950' onClick={()=>setColor("bg-pink-900")}>Pink</button>
+            <button className='bg-purple-800' onClick={()=>setColor("bg-purple-900")}>Purple</button>
         </div>
       </div>
     </div>
